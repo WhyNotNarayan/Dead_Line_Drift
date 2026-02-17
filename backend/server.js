163489452b3747c8ffb2d03e019fba01d0164f0e
Narayan Gawade <narayangawade684@gmail.com>
2026-02-17 22:04:49 +0530

    backend file

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/deadlinedrift')
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
